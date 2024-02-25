5e47c843cf4bec33e2eccd767f72d3ab4dcc1fd1
Bozhidar Nemkski <bojonemski@gmail.com>
2024-02-25 16:01:16 +0200

    refactor(local-env): set up local env for working with supergraph

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 import { ApolloServer } from '@apollo/server';
+import { buildSubgraphSchema } from '@apollo/subgraph';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import gql from 'graphql-tag';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+type User = {
+  id: string;
+  username: string;
+};
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+const typeDefs = gql`
+  extend schema
+    @link(
+      url: "https://specs.apollo.dev/federation/v2.0"
+      import: ["@key", "@shareable"]
+    )
+
+  type Query {
+    me: User
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
+  type User @key(fields: "id") {
+    id: ID!
+    username: String
   }
 `;
 
@@ -34,15 +38,23 @@ const books = [
 
 const resolvers = {
   Query: {
-    books: () => books,
+    me() {
+      return { id: '1', username: '@ava' };
+    },
+  },
+  User: {
+    __resolveReference(
+      user: User,
+      { fetchUserById }: { fetchUserById: (id: string) => User },
+    ) {
+      return fetchUserById(user.id);
+    },
   },
 };
-
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
 });
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
@@ -50,7 +62,7 @@ const server = new ApolloServer({
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: 4001 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
