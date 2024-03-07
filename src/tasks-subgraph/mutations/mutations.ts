a74064619911f8fbe1b5618df2621f3924de339f
Bozhidar Nemkski <bojonemski@gmail.com>
2024-03-07 11:25:13 +0200

    feat(week): add mutation for updating the total tasks completed

--- src/tasks-subgraph/mutations/mutations.ts
+++ src/tasks-subgraph/mutations/mutations.ts
@@ -1,9 +1,11 @@
 import deleteTask from './delete-task';
 import createTask from './create-task';
 import editTask from './edit-task';
+import updateTotalTasksCompleted from './update-total-tasks-completed';
 
 export default {
   deleteTask,
   createTask,
   editTask,
+  updateTotalTasksCompleted,
 };
---
+++ src/tasks-subgraph/mutations/update-total-tasks-completed.ts
@@ -0,0 +1,33 @@
+import tasksState from '../mock/tasks-data';
+
+export default function updateTotalTasksCompleted(
+  _: undefined,
+  args: { weekId: string; increase: boolean },
+) {
+  const { weekId, increase } = args;
+  const weeklyTasks = tasksState.find(
+    (weeklyTasks) => weeklyTasks.id === weekId,
+  );
+
+  if (!weeklyTasks) {
+    return {
+      success: false,
+      message: 'Week not found',
+      code: '400',
+      week: null,
+    };
+  }
+
+  if (increase) {
+    weeklyTasks.tasksCompleted += 1;
+  } else {
+    weeklyTasks.tasksCompleted -= 1;
+  }
+
+  return {
+    success: true,
+    message: 'Total tasks completed updated successfully',
+    code: '200',
+    week: weeklyTasks,
+  };
+}
