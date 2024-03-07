82b697b06fbd6e7cdbb022d664fc2512ed882825
Bozhidar Nemkski <bojonemski@gmail.com>
2024-03-07 11:09:20 +0200

    feat(edit-task): create edit task mutation

---
+++ src/tasks-subgraph/mutations/edit-task.ts
@@ -0,0 +1,44 @@
+import { tasksData } from '../mock';
+import { DayOfWeek, Task } from '../types';
+
+export default function editTask(
+  _: undefined,
+  args: { weekId: string; dayOfWeek: DayOfWeek; taskId: string; task: Task },
+) {
+  const { task, weekId, taskId, dayOfWeek } = args;
+  const weeklyTasks = tasksData.find(
+    (weeklyTasks) => weeklyTasks.id === weekId,
+  );
+
+  if (!weeklyTasks) {
+    return {
+      success: false,
+      message: 'Week not found',
+      code: '400',
+      task: null,
+    };
+  }
+
+  const dayToLowerCase = dayOfWeek.toLowerCase() as DayOfWeek;
+
+  const taskToEditIndex = weeklyTasks.tasks[dayToLowerCase].findIndex(
+    (task: Task) => task.id === taskId,
+  );
+
+  if (taskToEditIndex === -1) {
+    return {
+      success: false,
+      message: 'Task not found',
+      code: '400',
+      task: null,
+    };
+  }
+
+  weeklyTasks.tasks[dayToLowerCase][taskToEditIndex] = task;
+  return {
+    success: true,
+    message: 'Task edited successfully',
+    code: '200',
+    task: weeklyTasks.tasks[dayToLowerCase][taskToEditIndex],
+  };
+}
--- src/tasks-subgraph/mutations/mutations.ts
+++ src/tasks-subgraph/mutations/mutations.ts
@@ -1,7 +1,9 @@
-import deleteTask from './deleteTask';
-import createTask from './createTask';
+import deleteTask from './delete-task';
+import createTask from './create-task';
+import editTask from './edit-task';
 
 export default {
   deleteTask,
   createTask,
+  editTask,
 };
