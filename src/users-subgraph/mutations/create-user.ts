2939b553a01118f714073c81cbed9b6287950f6b
Bozhidar Nemski <bojonemski@gmail.com>
2024-06-02 15:58:23 +0300

    feat(create-user): create createUesr mutation and create user in firebase

--- src/users-subgraph/mutations/create-user.ts
+++ src/users-subgraph/mutations/create-user.ts
@@ -1,1 +1,54 @@
-export default function createUser() {}
+import { Collection } from 'mongodb';
+import { User } from '../types';
+import admin from '../firebase-config';
+import { FirebaseError } from 'firebase-admin/lib/utils/error';
+import { messaging } from 'firebase-admin';
+
+export default async function createUser(
+  _: undefined,
+  args: {
+    userData: {
+      email: string;
+      password: string;
+      photoUrl: string;
+    };
+  },
+  {
+    users,
+  }: {
+    users: Collection<User>;
+  },
+) {
+  const { userData } = args;
+  const { email, password, photoUrl } = userData;
+
+  try {
+    const response = await admin.auth().createUser({ email, password });
+  } catch (err) {
+    if (
+      (err as FirebaseError).message ===
+      'The email address is already in use by another account.'
+    ) {
+      return {
+        code: '400',
+        success: false,
+        message: 'The email address is already in use by another account.',
+        user: null,
+      };
+    } else {
+      return {
+        code: '400',
+        success: false,
+        message: 'User creation fail.',
+        user: null,
+      };
+    }
+  }
+
+  return {
+    code: '200',
+    message: 'User created successfullly',
+    success: true,
+    user: {},
+  };
+}
